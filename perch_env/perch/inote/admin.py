608c8a775b2763fe11353847438bc993f920ef49
asinajiang <2021425459@qq.com>
2018-12-09 16:01:51 +0800

    app inote init

--- perch_env/perch/inote/admin.py
+++ perch_env/perch/inote/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ArticleCate, Article
 
-# Register your models here.
+
+@admin.register(ArticleCate)
+class ArticleCateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cate_name')
+    ordering = ('id', 'cate_name')
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'last_update_time')
+    ordering = ('id', )
+
--- perch_env/perch/inote/models.py
+++ perch_env/perch/inote/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class ArticleCate(models.Model):
+    cate_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.cate_name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)  # 表示Article删除时，不删除对应的作者
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_update_time = models.DateTimeField(auto_now=True)
+    article_cate = models.ManyToManyField(ArticleCate)
+
+    def __str__(self):
+        return "Article: %s" % self.title
+
+
+
+
